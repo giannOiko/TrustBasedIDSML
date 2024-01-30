42f60ed07dd85e68dedda8da9d1177e157cfe9d6
giannOiko <152167513+giannOiko@users.noreply.github.com>
2024-01-30 02:45:53 +0200

    Update extractDataset.sh

--- extractDataset.sh
+++ extractDataset.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Specify the source and target file paths
+
 temp_file="temp_file.csv"
 dataset="dataset.csv"
 
+# remove first line
 for file in "$@" 
 do
 
@@ -12,12 +13,12 @@ do
 done
 rm "$temp_file"
 
-# Input and output file paths
+
 labels="labels.csv"
 temp_file1="temp.csv"
 
 # Column to extract
-column_to_extract="15"  # Change this to the column number you want to extract (1-indexed)
+column_to_extract="15"  
 
 # Check if input file exists
 if [ ! -f "$dataset" ]; then
@@ -30,6 +31,5 @@ cut -d',' -f"$column_to_extract" "$dataset" > "$labels"
 
 awk -v col="$column_to_extract" -F',' '{ for (i = 1; i <= NF; i++) if (i != col) printf "%s%s", $i, (i < NF ? "," : ""); print "" }' "$dataset" > "$temp_file1" && mv "$temp_file1" "$dataset"
 
-echo "Column $column_to_extract successfully extracted to $labels."
 
 
