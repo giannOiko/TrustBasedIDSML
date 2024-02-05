56fa4c9d6c964b5f07d488a78eba25675ef053eb
giannOiko <152167513+giannOiko@users.noreply.github.com>
2024-02-05 02:07:31 +0200

    Add files via upload

--- extractDataset.sh
+++ extractDataset.sh
@@ -14,22 +14,46 @@ done
 rm "$temp_file"
 
 
+# column extraction
 labels="labels.csv"
+num_labels="num_labels.csv"
 temp_file1="temp.csv"
 
-# Column to extract
-column_to_extract="15"  
-
-# Check if input file exists
-if [ ! -f "$dataset" ]; then
-    echo "Error: Input file '$dataset' not found."
-    exit 1
-fi
+column_to_extract1="4"
+column_to_extract2="5" 
+column_to_extract3="10" 
+column_to_extract4="11" 
+column_to_extract5="15"
+percentage="80" 
 
 # Extract the specified column and create a new CSV file
-cut -d',' -f"$column_to_extract" "$dataset" > "$labels"
+cut -d',' -f"$column_to_extract5" "$dataset" > "$labels"
 
-awk -v col="$column_to_extract" -F',' '{ for (i = 1; i <= NF; i++) if (i != col) printf "%s%s", $i, (i < NF ? "," : ""); print "" }' "$dataset" > "$temp_file1" && mv "$temp_file1" "$dataset"
+#convert labels into  and 1s
+sed 's/malicious/1/g; s/benign/0/g; s/outlier/1/g' "$labels" > "$num_labels"
+rm "$labels"
+
+#extract undesired columns
+awk -v col1="$column_to_extract1" -v col2="$column_to_extract2" -v col3="$column_to_extract3" -v col4="$column_to_extract4" -v col5="$column_to_extract5" -F',' '{ for (i = 1; i <= NF; i++) if (i != col1 && i != col2 && i != col3 && i != col4 && i != col5) printf "%s%s", $i, (i < NF ? "," : ""); print "" }' "$dataset" > "$temp_file1" && mv "$temp_file1" "$dataset"
 
 
+# split file 80/20
+training="training.csv"
+testing="testing.csv"
+training_labels="training_labels.csv"
+testing_labels="testing_labels.csv"
 
+
+total_lines=$(wc -l < "$dataset")
+lines_to_keep1=$(awk -v p="$percentage" 'BEGIN{print int(p * '$total_lines' / 100)}')
+lines_to_keep2=$((total_lines - lines_to_keep1))
+
+
+head -n "$lines_to_keep1" "$dataset" > "$training"
+tail -n "$lines_to_keep2" "$dataset" > "$testing"
+
+head -n "$lines_to_keep1" "$num_labels" > "$training_labels"
+tail -n "$lines_to_keep2" "$num_labels" > "$testing_labels"
+
+rm "$dataset"
+rm "$num_labels"
